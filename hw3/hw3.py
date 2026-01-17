a4f46987fea7bbd73f52b378b1dca7669ac9aa79
Anne Rogers <amr@cs.uchicago.edu>
2026-01-17 08:58:06 -0600

    Added materials for Homework 3

---
+++ hw3/hw3.py
@@ -0,0 +1,76 @@
+"""
+CMSC 14100
+Winter 2026
+Homework #3
+
+We will be using anonymous grading, so please do NOT include your name
+in this file
+
+People Consulted:
+   List anyone (other than the course staff) that you consulted about
+   this assignment.
+
+Online resources consulted:
+   List the URL of any online resources other than the course text and
+   the official Python language documentation that you used to complete
+   this assignment.
+"""
+
+def find_relative_return(current_day_price, prev_day_price):
+    """
+    Calculates the relative return for a pair of prices.
+
+    The return is defined as the ratio of the price on the specified
+    day to the price on the previous day.
+
+    Args:
+        current_day_price (float): the current day's price
+        prev_day_price (float): the previous day's price
+
+    Returns (float | None):
+            - Returns the ratio of current day to previous day.
+            - Returns None if either price is 0 or negative.
+    """
+    raise ValueError("TODO: Exercise 1 Not Complete")
+
+
+def find_value_at_risk(current_day_price, prev_day_price):
+    """
+    Your docstring goes here
+    """
+    raise ValueError("TODO: Exercise 2 Not Complete")
+
+
+def get_stock_rating(return_on_investment, avg_value_at_risk):
+    """
+    Your docstring goes here
+    """
+    raise ValueError("TODO: Exercise 3 Not Complete")
+
+
+def compare_stock_ratings(ratings_1, ratings_2):
+    """
+    Your docstring goes here
+    """
+    raise ValueError("TODO: Exercise 4 Not Complete")
+
+
+def find_period_return(prices):
+    """
+    Your docstring goes here
+    """
+    raise ValueError("TODO: Exercise 5 Not Complete")
+
+
+def find_avg_var(prices):
+    """
+    Your docstring goes here
+    """
+    raise ValueError("TODO: Exercise 6 Not Complete")
+
+
+def find_stock_rating_from_prices(prices):
+    """
+    Your docstring goes here
+    """
+    raise ValueError("TODO: Exercise 7 Not Complete")
